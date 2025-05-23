27dbb6778e4011edfdc7b49af4b713664af37b57
simon odeh <simonodeh36@gmail.com>
2025-05-23 16:09:15 +0100

    configured prisma

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
+const prisma = new PrismaClient();
 const app = express();
 
 app.use(cors({ origin: "http://localhost:5173" }));
